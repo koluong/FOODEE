50ed87cdea73b127f4d1b62e02cfed941ef99544
koluong <kennyk94@live.com>
2017-04-06 11:52:03 -0700

    Create recipe detail component

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -9,7 +9,9 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes:Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test description.', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
     new Recipe('A Test Recipe', 'This is a test description.', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+
   ];
 
   constructor() { }
